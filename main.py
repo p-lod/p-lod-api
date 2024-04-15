2668b2437d79b412255af8c06913c855df58290e
Sebastian Heath <sebastian.heath@gmail.com>
2024-04-15 00:12:38 -0400

    turtle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import plodlib
-from typing import Union
+from typing import Union, Any, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response, RedirectResponse
 
 app = FastAPI()
 
@@ -22,13 +23,29 @@ def not_implemented():
     return """Nothing here. Try /id/pompeii . See /docs for documentation. One quick note: 
     In general you pass either simple ids ('pompeii') or urns ('urn:p-lod:id:pompeii') to the API."""
 
+class CustomTTLResponse(Response):
+    media_type = "text/turtle"
+    def render(self, content: Any) -> bytes:
+        return content
+
+@app.get('/id/{p_lod_id}.ttl', response_class=CustomTTLResponse)
+def get_ttl(p_lod_id: str):
+    """Return an RDF representation of a P-LOD ID serialized as Turtle and with mimetype set to text/turtle."""
+    rdf_data = plodlib.PLODResource(p_lod_id.replace('urn:p-lod:id:','')).rdf_describe()
+    response = Response(content=rdf_data, media_type="text/turtle")
+    return response
+
+
 @app.get("/id/{p_lod_id}")
-def information_about_an_id(p_lod_id: str, q: Union[str, None] = None):
+def information_about_an_id(p_lod_id: str, q: Union[str, None] = None, accept: Annotated[str | None, Header()] = None):
     """Return an array of dictionaries describing a P-LOD ID. The key of each dictionary is the predicate, and the value is the object.
     
     The format of the returned JSON is under development and may change.
-    """
-    
+    """   
+
+    if accept == 'text/turtle':
+        return RedirectResponse(url=f'/id/{p_lod_id}.ttl')
+
     r = plodlib.PLODResource(p_lod_id.replace('urn:p-lod:id:',''))
 
     return [{row.name:row['o']} for i,row in r._id_df.iterrows()]
